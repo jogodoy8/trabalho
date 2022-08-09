62e2e360fda566f5591a5cbb1632c28d2f96e97d
GacAcassio <100379848+GacAcassio@users.noreply.github.com>
2022-08-09 19:43:13 -0300

    Update trabalho1.cpp

--- trabalho1.cpp
+++ trabalho1.cpp
@@ -14,162 +14,46 @@ RGA: 202115310001
      202111310049
 Data da entrega: 10/08/2022
 **************************************************************/
-void imprimir(float tabela[][8], int k, int maiorFI)
+void imprimeModa(int tamanho,int amostra[])
 {
-	int i,j;
-	printf("    CI      CS      FI      XI      FR       FCA     FCI     FCS    \n");
-    	for(l=0;l<k;l++) 
-    	  {for(c=0;c<8;c++) 
-      	   {
-         	
-			 printf("    %.2f", tabela[l][c]);        
-           }
-        	 printf("\n\n");
-	   }
-	  
-	//Grafico
-	
-		int corGraf[k-1];
-		printf("\n\n\n");
-		for(i=0;i<k;i++)
-		{
-			corGraf[i]= rand()%9+1;	
-		}
-		for(j=maiorFI+1;j>=0;j--)
-		{
-	  	  for(i=0;i<k;i++)
-		{
-			if(tabela[i][2]>=j){
-				textcolor(corGraf[i]);
-				if(tabela[i][2]==j)
-				{
-					printf(" %.f ",tabela[i][2]);	
-				}else
-				{
-					printf("%c%c%c", 178, 178,178);	
-				}
-			}else{
-				printf("   ");
-			}
-		}
-		printf("\n");
-	}
-	
-	
-}
-int main()//programa principal
-{	
-	int k,l,c,cont=1,primeiro=0,aux,menor,i,at,atc;
-	int moda,a,j,v2[6],maior;
-	float soma=0,media=0,mediana=0;
-	int tamanho;
-	
-	printf("Olá, para iniciar a construção da sua tabela de frequência insira o numero de elementos da amostra:");
-	scanf("%d", &tamanho);
-	
-	int amostra[tamanho];
-	
-	for(i=0; i<tamanho; i++)
-	{
-		printf("Insira o elemento [%d] da amostra= ",i+1);
-		scanf("%d", &amostra[i]);
-		soma+=amostra[i];
-	}
-	
-	for(i=0;i<tamanho;i++)
-	{
-		printf(" %d", amostra[i]);
-	}		
-		
-	while (cont <= tamanho)
-	{
-		menor = primeiro;
-		for(i = primeiro + 1; i < tamanho; i++)
-		{
-			if(amostra[menor] > amostra[i])
-			{
-				menor = i;
-			}
-		}
-		aux = amostra[primeiro];
-		amostra[primeiro] = amostra[menor];
-		amostra[menor]=aux;
-		primeiro = primeiro + 1;
-		cont++;
-	}
-	
-	printf("\n");
-	
-	for(int i = 0; i < tamanho; i++)
-	{
-		printf(" %d", amostra[i]);
-	}
-	
-	printf("\n");
-	
-	k=((int)(ceil(sqrt(tamanho))));
-	float tabela[k][8];
-	printf("\nK e igual: %d",k);
-	at=amostra[tamanho-1]-amostra[0];
-	printf("\nAT e igual: %d",at);
-	atc=ceil(at/((float)k));
-	printf("\nATC e igual: %d",atc);
-	
-	//media
-	media=soma/tamanho;
-	printf("\nMedia: %.2f",media);
-	
-	//mediana
-	
-	if(tamanho%2==1)
-	{
-		mediana=amostra[(tamanho-1)/2];
-	}
-	else
-	{
-		mediana=((float)(amostra[(tamanho/2)-1]+amostra[tamanho/2]))/2;
-	}
-	
-	printf("\nMediana: %.2f",mediana);
-	
-	//moda
-
-	int quantRepet=1;
-	int acumulo=0;
-	for(i=1;i<tamanho;i++)
+	int i, j, quantRepet=1;
+	for(i=1; i<tamanho; i++)
 	{
 		if(amostra[i-1]!=amostra[i])
 		{
 			quantRepet++;
 		}
 	}
+	int acumulo=0, moda;
 	if(quantRepet==tamanho)
 	{
-		printf("\nNao ha moda");
-	}else{
+		printf("\n Nao ha moda");
+	}
+	else
+	{
 		int matRepet[2][quantRepet];
 		j=0;
 		for(i=1;i<tamanho;i++)
 		{
 			if((amostra[i-1]!=amostra[i])||((amostra[tamanho-1]==amostra[tamanho-2])&&(i==tamanho-1)))
 			{
-				matRepet[1][j]=amostra[i-1];
+				matRepet[0][j]=amostra[i-1];
 				if(j==0)
 				{
-					matRepet[2][j]=i;
+					matRepet[1][j]=i;
 					moda=j;
 				}else{
-					acumulo+=matRepet[2][j-1];
-					if((amostra[tamanho-1]==amostra[tamanho-2])&&(i==tamanho-1))
+					acumulo+=matRepet[1][j-1];
+					if(amostra[tamanho-1]!=amostra[tamanho-2])
 					{
-						acumulo--;
+						matRepet[1][j+1]=1;	
 					}
-					if(amostra[tamanho-1]==amostra[tamanho-2])
+					else if(matRepet[1])
 					{
-						matRepet[2][j+1]=1;	
+						acumulo--;	
 					}
-					matRepet[2][j]=i-acumulo;
-					if(matRepet[2][moda]<matRepet[2][j])
+					matRepet[1][j]=i-acumulo;
+					if(matRepet[1][moda]<matRepet[1][j])
 					{
 						moda=j;
 					}
@@ -179,35 +63,33 @@ int main()//programa principal
 		}
 		int outrasModas=0;
 		for(j=0;j<quantRepet;j++)
-		{
-			printf("\nR=%d",matRepet[2][j]);
-		 	if(matRepet[2][moda]==matRepet[2][j])
+		{	
+		 	if(matRepet[1][moda]==matRepet[1][j])
 					{
 						outrasModas++;
-						printf("\nM=%d",outrasModas);
 					}
 		}
 		int vetOutrasModas[outrasModas];
 		i=0;
-		for(j=0;j<quantRepet;j++)
+		for(j=0; j<quantRepet; j++)
 		{
-		 	if(matRepet[2][moda]==matRepet[2][j])
+		 	if(matRepet[1][moda]==matRepet[1][j])
 					{
-						 vetOutrasModas[i]=matRepet[1][j];
+						 vetOutrasModas[i]=matRepet[0][j];
 						 i++;	
 					}
 		}
-		if(outrasModas* matRepet[2][moda]==tamanho)
+		if(outrasModas* matRepet[1][moda]==tamanho)
 		{
-			printf("\nNao ha moda");
+			printf("\n Nao ha moda");
 		}else{
 			switch (outrasModas)
 			{
 				case 1:
-					printf("\nA moda e %d", matRepet[1][moda]);
+					printf("\n A moda e %d", matRepet[0][moda]);
 					break;
 				default:
-					printf("\nAs modas sao  ");
+					printf("\n As modas sao  ");
 					for(i=0;i<outrasModas;i++)
 					{
 						(i!=outrasModas-1&&i!=0)? printf(", "): (i==outrasModas-1)? printf(" e "): printf("");
@@ -217,48 +99,158 @@ int main()//programa principal
 					break;	
 			}	
 		}
-		
-	}
-	//definindo ca tagbela
+	}			
+}
+int main()//programa principal
+{	
+	int tamanho;
+	printf("Ola, para iniciar a construção da sua tabela de frequência insira o numero de elementos da amostra:");
+	scanf("%d", &tamanho);
+	int amostra[tamanho];
 	
-	acumulo=0;
-	int maiorFI=0;
-	for(l=0;l<k;l++)
+	int i, j;
+	float soma=0;
+	for(i=0; i<tamanho; i++)
 	{
-		tabela[l][0]=amostra[0]+acumulo;
+		printf("Insira o elemento [%d] da amostra= ", i+1);
+		scanf("%d", &amostra[i]);
+		soma+=amostra[i];
+	}
+	
+	//Ordenação	
+	int cont=1, primeiro=0, aux, menor, maior;			
+	while(cont<=tamanho)
+	{
+		menor=primeiro;
+		for(i=primeiro+1; i<tamanho; i++)
+		{
+			if(amostra[menor]>amostra[i])
+			{
+				menor=i;
+			}
+		}
+		aux=amostra[primeiro];
+		amostra[primeiro]=amostra[menor];
+		amostra[menor]=aux;
+		primeiro=primeiro + 1;
+		cont++;
+	}
+	
+	//definindo valores
+	int k, at, atc;
+	k=((int)(ceil(sqrt(tamanho))));
+	at=amostra[tamanho-1]-amostra[0];
+	atc=ceil(at/((float)k));
+	
+	//media
+	float media=0;
+	media=soma/tamanho;
+	
+	//mediana
+	float mediana=0;
+	if(tamanho%2==1)
+	{
+		mediana=amostra[(tamanho-1)/2];
+	}
+	else
+	{
+		mediana=((float)(amostra[(tamanho/2)-1]+amostra[tamanho/2]))/2;
+	}
+		
+	//definindo a tabela	
+	float tabela[k][8];
+	int acumuloAux=0, acumulo=0, maiorFI=0;
+	for(i=0; i<k; i++)
+	{
+		tabela[i][0]=amostra[0]+acumulo;
 		acumulo+=atc;
-		tabela[l][1]=tabela[l][0]+atc-1;
+		tabela[i][1]=tabela[i][0]+atc-1;
 		if (tabela[k-1][1]<amostra[tamanho-1])
 		{
 			tabela[k-1][1]=amostra[tamanho-1];
 		}		
-		tabela[l][3]=(tabela[l][0]+tabela[l][1])/2;
-		tabela[l][6]=tabela[l][0]-0.5;
-		tabela[l][7]=tabela[l][1]+0.5;
-		
-	}
-	acumulo=0;
-	for(l=0;l<k;l++)
-	{   
-	    tabela[l][2]=0;
-		for(i=0;i<tamanho;i++)
+		tabela[i][3]=(tabela[i][0]+tabela[i][1])/2;
+		tabela[i][6]=tabela[i][0]-0.5;
+		tabela[i][7]=tabela[i][1]+0.5;	
+		tabela[i][2]=0;
+		for(j=0; j<tamanho;  j++)
 		{
-			if(amostra[i]>=tabela[l][0]&&amostra[i]<=tabela[l][1])
+			if(amostra[j]>=tabela[i][0]&&amostra[j]<=tabela[i][1])
 			{
-				tabela[l][2]++;
+				tabela[i][2]++;
 			}
 		}
-		if(maiorFI<tabela[l][2])
+		if(maiorFI<tabela[i][2])
 		{
-			maiorFI=tabela[l][2];
+			maiorFI=tabela[i][2];
 		}
-		tabela[l][4]=(tabela[l][2]*100)/tamanho;
-		acumulo+=tabela[l][2];
-		tabela[l][5]=acumulo;
+		tabela[i][4]=(tabela[i][2]*100)/tamanho;
+		acumuloAux+=tabela[i][2];
+		tabela[i][5]=acumuloAux;	
 	}
-	printf("\nMaiorFI: %d", maiorFI);
-	printf("\n\n");
-	imprimir(tabela, k, maiorFI);
-	
+
+	//imprimindo o resultado
+	system("cls");
+	gotoxy(40, 3);
+	printf("AMOSTRA");
+	gotoxy(43-tamanho*2, 5);
+	for(i=0; i<tamanho; i++)
+	{
+		printf(" %3.d", amostra[i]);
+	}
+	printf("\n\n Media amostral: %4.2f \n Mediana: %4.2f", media, mediana);
+	imprimeModa(tamanho, amostra);
+	printf("\n");
+	printf(" _________________________________________________________________________________________\n");
+	printf("|                     |          |          |           |          |                      |\n");
+	printf("|       CLASSES       |          |          |           |          | FRONTEIRA DE CLASSES |\n");
+	printf("|_____________________|__________|__________|___________|__________|______________________|\n");
+	printf("|          |          |          |          |           |          |          |           |\n");
+	printf("|    CI    |   CS     |    FI    |    XI    |    FR     |   FCA    |   FCI    |    FCS    |\n");
+	printf("|__________|__________|__________|__________|___________|__________|__________|___________|\n");
+	printf("|          |          |          |          |           |          |          |           |\n");
+    for(i=0; i<k; i++) 
+    {
+    	printf("|");
+		for(j=0; j<8; j++) 
+		{ 
+    		(j==4)? printf("%8.2f%%  |", tabela[i][j]) : (j==7)? printf("% 8.2f   |", tabela[i][j]) : printf("%8.2f  |", tabela[i][j]);        
+		}
+        printf("\n|__________|__________|__________|__________|___________|__________|__________|___________|\n");
+        (i!=k-1)? printf("|          |          |          |          |           |          |          |           |\n") :printf("\n");
+	}
+	  
+	//Grafico
+	printf("\n");
+	printf("                                      HISTOROGRAMA   \n\n");
+	int corGraf[k-1];
+	for(i=0; i<k; i++)
+	{
+		corGraf[i]=rand()%9+1;	
+	}
+	for(j=maiorFI+1; j>=0; j--)
+	{
+		gotoxy(43-k, 21+maiorFI+k*3-j);
+	    for(i=0; i<k; i++)
+		{   
+			if(tabela[i][2]>=j){
+				textcolor(corGraf[i]);
+				if(tabela[i][2]==j)
+				{
+					printf(" %.f ", tabela[i][2]);	
+				}
+				else
+				{
+					printf("%c%c%c", 178, 178, 178);	
+				}
+			}
+			else
+			{
+				printf("   ");
+			}
+		}
+		textcolor(7);
+		printf("\n");
+	}	
 	return 0;
 }
